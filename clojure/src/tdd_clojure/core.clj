ffd8834112442b63ace51673d4dc57ed85fe1ee1
Sören Blom <soeren.blom@infinitec.solutions>
2019-11-14 09:49:02 +0100

    Code including requirement 5

--- clojure/src/tdd_clojure/core.clj
+++ clojure/src/tdd_clojure/core.clj
@@ -4,9 +4,9 @@
 (defn- reduce-equal [e1 e2]
   (if (= e1 e2) e1 false))
 
-(defn- diagonal-lr [pos row] (get row pos))
+(defn diagonal-lr [pos row] (get row pos))
 
-(defn- diagonal-rl [pos row] (get row (- (count row) pos 1)))
+(defn diagonal-rl [pos row] (get row (- (count row) pos 1)))
 
 (defn- transpose [fields] (apply mapv vector fields))
 
@@ -17,9 +17,17 @@
   [fields]
   (some keyword? (doall (map #(reduce reduce-equal %) fields))))
 
-(defn row-taken? [fields] (column-taken? (transpose fields)))
+(defn row-taken?
+  "Checks if all `fields` in a column have the same value.
+  If so, that value is returned, otherwise `false`."
+  [fields]
+  (column-taken? (transpose fields)))
 
-(defn diagonal-taken? [diagonal fields]
+;; TODO: Refactor so that the predicate has the two diagonals built-in.
+(defn diagonal-taken?
+  "Checks if all fields diagonally have the same value. The `diagonal` is
+  defined as a function that returns the column index for the diagonal."
+  [diagonal fields]
   (reduce reduce-equal
           (map-indexed diagonal fields)))
 
@@ -32,3 +40,20 @@
              (row-taken? fields)
              (diagonal-taken? diagonal-lr fields)
              (diagonal-taken? diagonal-rl fields))))
+
+(defn field-taken? [fields nrow ncol]
+  "Determines if the field of a `field` at the row `nrow` and column `ncol` is
+  taken (i.e. not `nil`. If so, it returns `true` and `false` otherwise.
+  The `field` is expected to be a vector of vectors."
+  (if (some? (-> fields (get nrow) (get ncol))) true false))
+
+(defn update-field [fields val nrow ncol]
+  "'Updates' the value in field of a `field` with `val`."
+  (let [row  (get fields nrow)
+        field (get row ncol)]
+   (-> (assoc fields nrow (assoc row ncol val)))))
+
+(defn take-field! [board player nrow ncol]
+    (if (not (field-taken? @board nrow ncol))
+      (swap! board update-field player nrow ncol)
+      nil))
--- clojure/test/tdd_clojure/core_test.clj
+++ clojure/test/tdd_clojure/core_test.clj
@@ -1,11 +1,14 @@
 (ns tdd-clojure.core-test
   (:require [clojure.test :refer :all]
+            [eftest.runner :as ef]
             [tdd-clojure.core :as tdd]))
 
 ;; REQ: A game of tic-tac-toe is over if all felds are covered
 ;;  -> There is a collection of fields
 ;;  -> Fields can be marked as taken
-(def all-fields-taken [[:p1 :p2 :p3] [:p1 :p1 :p2] [:p1 :p1 :p2]])
+(def all-fields-taken [[:p1 :p2 :p3]
+                       [:p1 :p1 :p2]
+                       [:p1 :p1 :p2]])
 
 (deftest game-over-when-all-fields-taken
   (is (= true (tdd/all-taken? all-fields-taken)))
@@ -28,8 +31,8 @@
 
 (deftest game-over-when-column-taken-by-player
   (is (= true
-         (tdd/column-taken? one-column-taken-by-same-player))
-   "one column taken by a player means game over"))
+         (tdd/column-taken? one-column-taken-by-player)))
+  "one column taken by a player means game over")
 
 ;; REQ: a game is over when all fields in a row are taken by a player
 (def one-row-taken-by-player [[:p1 :p2 :p3]
@@ -39,7 +42,7 @@
 
 (deftest game-over-when-row-taken-by-player
   (is (= true
-         (tdd/row-taken? one-row-taken-by-same-player)))
+         (tdd/row-taken? one-row-taken-by-player)))
   "one row taken by a player means game over")
 
 ;; REQ: a game is over when all fields in a diagonal are taken by a player
@@ -69,3 +72,31 @@
   (is (= false
          (tdd/diagonal-taken? tdd/diagonal-lr
                               all-fields-taken))))
+
+;;a player can take a field if not already taken
+
+(def board-with-empty-fields [[:p1 nil nil]
+                              [nil :p1 :p1]
+                              [:p1 :p1 :p1]])
+
+(deftest update-empty-field
+  (testing "update on empty field returns updated field"
+    (is (= [[:p1 :p1 nil] ;at[0][1] expect change to :p1
+            [nil :p1 :p1]
+            [:p1 :p1 :p1]]
+           (tdd/update-field board-with-empty-fields :p1 0 1))))
+  (testing "update on taken field returns nil"
+    (is (= board-with-empty-fields
+           (tdd/update-field board-with-empty-fields :p1 2 2)))))
+
+(deftest take-field-allows-update-on-field-only-first-time
+  (let [board (atom board-with-empty-fields)
+        resulting-field [[:p1 :p1 nil]
+                         [nil :p1 :p1]
+                         [:p1 :p1 :p1]]]
+      (is (= resulting-field
+             (tdd/take-field! board :p1 0 1)))
+      (is (= nil
+             (tdd/take-field! board :p1 0 1)))
+      (is (= resulting-field
+             @board))))
