cca7fb26d48bdeac1c0bcd5eec6dbcedbaa775c1
Sören Blom <soeren.blom@infinitec.solutions>
2019-11-25 00:05:38 +0100

    Restarted the exercise. Requirement 1.

--- clojure/project.clj
+++ clojure/project.clj
@@ -4,7 +4,6 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0"]
-                 [proto-repl "0.3.1"]
                  [clj-kondo "2019.11.03"]]
   :aliases {"clj-kondo" ["run" "-m" "clj-kondo.main"]}
   :plugins [[lein-kibit "0.1.7"]
--- clojure/src/tdd_clojure/core.clj
+++ clojure/src/tdd_clojure/core.clj
@@ -1,68 +1,8 @@
-(ns tdd-clojure.core
-  (:require [clojure.set :as set]))
+(ns tdd-clojure.core)
 
-
-(defn- reduce-equal [e1 e2]
-  (if (= e1 e2) e1 false))
-
-(defn diagonal-lr [pos row] (get row pos))
-
-(defn diagonal-rl [pos row] (get row (- (count row) pos 1)))
-
-(defn- transpose [fields] (apply mapv vector fields))
-
-(defn all-taken? [fields] (empty? (fields :empty)))
-
-(defn column-taken?
-  "Checks if all `fields` in a column have the same value.
-  If so, that value is returned, otherwise `false`."
-  [fields]
-  (some keyword? (doall (map #(reduce reduce-equal %) fields))))
-
-(defn split-rows [moves]
-  (let [sorted (sort moves)
-        [r1 cdr] (split-with #(< % 4) sorted)
-        [r2 r3] (split-with #(< % 7) cdr)]
-    [r1 r2 r3]))
-
-(defn row-taken? [moves]
-  "Tests whether any row is taken by either of the players."
-  (let [{f-p1 :p1 f-p2 :p2} moves]
-    (letfn [(full-row? [fields]
-                       (some true? (map #(= 3 (count %))
-                                        (split-rows fields))))]
-      (or (full-row? f-p1) (full-row? f-p2)))))
-
-;; TODO: Refactor so that the predicate has the two diagonals built-in.
-(defn diagonal-taken?
-  "Checks if all fields diagonally have the same value. The `diagonal` is
-  defined as a function that returns the column index for the diagonal."
-  [diagonal fields]
-  (reduce reduce-equal
-          (map-indexed diagonal fields)))
-
-(defn game-over? [fields]
-  (true? (or (all-taken? fields)
-             (column-taken? fields)
-             (row-taken? fields)
-             (diagonal-taken? diagonal-lr fields)
-             (diagonal-taken? diagonal-rl fields))))
-
-(defn field-taken?
-  "Determines if the `field` at the row `nrow` and column `ncol` is taken.
-  Taken is defined as being not `nil`. If so, it returns `true` and `false` otherwise.
-  The `field` is expected to be a vector of vectors."
-  [fields nrow ncol]
-  (if (some? (-> fields (get nrow) (get ncol))) true false))
-
-(defn update-field
-  "'Updates' the value in field of a `field` with `val`."
-  [fields val nrow ncol]
-  (let [row  (get fields nrow)
-        field (get row ncol)]
-    (-> (assoc fields nrow (assoc row ncol val)))))
-
-(defn take-field! [board player nrow ncol]
-    (if (not (field-taken? @board nrow ncol))
-      (swap! board update-field player nrow ncol)
-      nil))
+(defn game-over-all-fields?
+  "Checks if a game is over due to all fields being taken. 
+  `fields-taken` is the number of fields taken at this point in time."
+  [fields-taken]
+  (let [no-of-fields-in-game 9]
+    (>= fields-taken no-of-fields-in-game fields-taken)))
