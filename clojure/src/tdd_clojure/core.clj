10abcf2ae644771859903ffb2133e80856e588dc
Sören Blom <soeren.blom@infinitec.solutions>
2019-11-25 09:24:30 +0100

    Restarted. Req3. After inital test-writing

--- clojure/src/tdd_clojure/core.clj
+++ clojure/src/tdd_clojure/core.clj
@@ -7,9 +7,13 @@
   (let [no-of-fields-in-game 9]
     (>= fields-taken no-of-fields-in-game fields-taken)))
 
-(defn game-over-column?
-  "Checks if a game is over due to any column being taken by a player.
-  `col-taken-vec` is the vector of results of the checking all three columns
-  for being taken by the same player."
-  [col-taken-vec]
-  (not (nil? (some true? col-taken-vec))))
+(defn game-over-groups-of-fields?
+  "Checks if a game is over due to a particular group of fields being taken by
+  a player. Such groups can be columns, rows and diagonals
+  `taken-vec` is the vector of results checking all groups of one type for
+  fields taken."
+  [taken-vec]
+  (->> taken-vec
+       (some true?)
+       (nil?)
+       (not)))
--- clojure/test/tdd_clojure/core_test.clj
+++ clojure/test/tdd_clojure/core_test.clj
@@ -17,8 +17,38 @@
   (let [fields-reduction-one-or-more-columns-taken-by-player [true false false]
         fields-reduction-no-column-taken-by-player [false false false]]
     (is (= true
-           (tdd/game-over-column? 
+           (tdd/game-over-groups-of-fields? 
               fields-reduction-one-or-more-columns-taken-by-player)))
     (is (= false
-           (tdd/game-over-column? 
+           (tdd/game-over-groups-of-fields? 
               fields-reduction-no-column-taken-by-player)))))
+
+;; Req3: A `game is over` when `all fields in a row` are `taken` by one `player`
+
+; After re-using `game-over-column?` for rows:
+; Test works without refactoring => We have said nothing about rows vs cols.
+; This will obviously be true for the diagonals: Given a vector with booleans
+;   that represent whether the diagonals are taken will lead to the same result.
+
+; What makes rows and cols different? The fields they are made of!
+;   In case of rows:  Top 3 fields    = Row 1
+;                     Mid 3 fields    = Row 2
+;                     Low 3 fields    = Row 3
+
+;   Whereas for cols: Left   3 fields = Col 1
+;                     Middle 3 fields = Col 2
+;                     Right  3 fields = Col 3
+
+; We have also not said anything about 'fields taken by player'.
+; What does it mean that a field is taken by player? How do we represent fields
+; and how do we represent their 'taken by' or 'not taken' status?
+
+(deftest game-over-when-one-row-taken-by-player
+  (let [fields-reduction-one-or-more-rows-taken-by-player [true false false]
+        fields-reduction-no-row-taken-by-player [false false false]]
+    (is (= true
+           (tdd/game-over-groups-of-fields? 
+            fields-reduction-one-or-more-rows-taken-by-player)))
+    (is (= false
+           (tdd/game-over-groups-of-fields? 
+            fields-reduction-no-row-taken-by-player)))))
